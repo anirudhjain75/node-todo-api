161de3204ab0519ac242e7cc86c091dc7450e625
Anirudh Jain <anirudh.jain@outlook.com>
2018-07-29 15:25:21 +0530

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b5ca811691ad6177aa19b09')
+  // }).toArray().then((docs) => {
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+  db.collection('Users').find().count().then((count)=>{
+    console.log(count);
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  })
+  // // db.close();
+});
